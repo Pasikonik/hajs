366216206bd160ede41b7d9409c4beff86f24e62
Pasikonik <blind.x6@gmail.com>
2016-03-03 18:19:15 +0100

    clean adapter

--- frontend/app/adapters/application.js
+++ frontend/app/adapters/application.js
@@ -1,30 +1,25 @@
+import Ember from 'ember';
 import DS from 'ember-data';
 import ENV from "../config/environment";
 import DataAdapterMixin from 'ember-simple-auth/mixins/data-adapter-mixin';
 
-export default  DS.JSONAPIAdapter.extend({
+const { get, $ } = Ember;
+
+export default DS.JSONAPIAdapter.extend(DataAdapterMixin, {
   namespace: 'api/v1',
   authorizer: 'authorizer:devise',
-  // if your rails app is on a different port from your ember app
-  // this can be helpful for development.
-  // in production, the host for both rails and ember should be the same.
-  host: ENV.host,
+  host: ENV.apiURL,
 
-  // allows the multiword paths in urls to be underscored
   pathForType: function(type) {
     let underscored = Ember.String.underscore(type);
     return Ember.String.pluralize(underscored);
   },
 
-  // allows queries to be sent along with a findRecord
-  // hopefully Ember / EmberData will soon have this built in
-  // ember-data issue tracked here:
-  // https://github.com/emberjs/data/issues/3596
   urlForFindRecord(id, modelName, snapshot) {
     let url = this._super(...arguments);
-    let query = Ember.get(snapshot, 'adapterOptions.query');
+    const query = get(snapshot, 'adapterOptions.query');
     if(query) {
-      url += '?' + Ember.$.param(query);
+      url += '?' + $.param(query);
     }
     return url;
   }
