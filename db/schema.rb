ce8602941e9d360ea63c4ece9978afbebffc7af4
Pasikonik <blind.x6@gmail.com>
2016-03-03 18:24:42 +0100

    little changes

--- db/schema.rb
+++ db/schema.rb
@@ -30,8 +30,9 @@ ActiveRecord::Schema.define(version: 20160213211006) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.string   "authentication_token"
-    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
