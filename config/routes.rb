a814a0da51694ba24e5f2ec0b4af423b58f8d069
Pasikonik <blind.x6@gmail.com>
2016-03-03 18:18:08 +0100

    use route to user api

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,10 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { sessions: 'sessions' }
 
   mount_ember_app :frontend, to: '/'
+
+  namespace :api do
+    namespace :v1 do
+      resources :users
+    end
+  end
 end
