1ca574dbbfb77a381a63c3dc2443b810f66b7e30
Pasikonik <blind.x6@gmail.com>
2016-03-06 22:57:13 +0100

    add workflow for deprecations

--- frontend/config/deprecation-workflow.js
+++ frontend/config/deprecation-workflow.js
@@ -2,6 +2,6 @@ window.deprecationWorkflow = window.deprecationWorkflow || {};
 window.deprecationWorkflow.config = {
   workflow: [
     { handler: "silence", matchMessage: "Using the injected `container` is deprecated. Please use the `getOwner` helper instead to access the owner of this object." },
-    { handler: "silence", matchMessage: "Support for the `ember-legacy-controllers` addon will end soon, please remove it from your application"}
+    { handler: "silence", matchMessage: "Support for the `ember-legacy-controllers` addon will end soon, please remove it from your application"},
   ]
 };
