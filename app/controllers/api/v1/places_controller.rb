3031f401e041b015b272000630260def60ecf78b
Pasikonik <blind.x6@gmail.com>
2016-03-06 22:47:56 +0100

    create place controller

---
+++ app/controllers/api/v1/places_controller.rb
@@ -0,0 +1,27 @@
+class Api::V1::PlacesController < BaseController
+  def index
+    render json: Place.all
+  end
+
+  def create
+    place = Place.new place_params
+    place.payer = current_user
+    place.members << current_user
+    if place.save
+      render json: place, status: :ok
+    else
+      binding.pry
+      render json: { errors: place.errors }, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    render json: Place.find(params[:id])
+  end
+
+  private
+
+  def place_params
+    params.require(:data).require(:attributes).permit(:name, :description, :address, :area, :capacity, :rent)
+  end
+end
