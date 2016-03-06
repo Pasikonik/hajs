510a936cbc33986a2b849ec1724ef913558d5200
Pasikonik <blind.x6@gmail.com>
2016-03-06 22:52:04 +0100

    add place model on frontend

---
+++ frontend/app/models/place.js
@@ -0,0 +1,14 @@
+import DS from 'ember-data';
+
+const { attr, belongsTo, hasMany } = DS;
+
+export default DS.Model.extend({
+  name: attr('string'),
+  description: attr('string'),
+  address: attr('string'),
+  area: attr('number'),
+  capacity: attr('number'),
+  rent: attr('number'),
+  payer: belongsTo('user'),
+  members: hasMany('user')
+});
