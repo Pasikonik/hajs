41f42af45432490f6c925ab3d96000952e9c4ba7
Pasikonik <blind.x6@gmail.com>
2016-03-07 19:28:18 +0100

    add foreign key

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160306221841) do
+ActiveRecord::Schema.define(version: 20160307181137) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -62,6 +62,7 @@ ActiveRecord::Schema.define(version: 20160306221841) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.string   "authentication_token"
+    t.integer  "place_id"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
