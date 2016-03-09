c7d620990b0e13d201d0a6251dd12d8c806b375d
Pasikonik <blind.x6@gmail.com>
2016-03-09 22:57:53 +0100

    update place serializer

--- app/serializers/place_serializer.rb
+++ app/serializers/place_serializer.rb
@@ -1,3 +1,6 @@
 class PlaceSerializer < ActiveModel::Serializer
   attributes :id, :name, :description, :address, :area, :capacity, :rent
+  has_many :users
+  has_many :bills, require: false
+  belongs_to :payer
 end
