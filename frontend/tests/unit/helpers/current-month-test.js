d4ee162743cf2c391d7b48186222f4df7a50654e
Pasikonik <blind.x6@gmail.com>
2016-03-09 23:03:06 +0100

    add some tests

---
+++ frontend/tests/unit/helpers/current-month-test.js
@@ -0,0 +1,10 @@
+import { currentMonth } from 'frontend/helpers/current-month';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | current month');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = currentMonth([42]);
+  assert.ok(result);
+});
