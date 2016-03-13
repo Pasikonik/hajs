e328dd2e03d520709a90b1cbc0587e8d494008a1
Pasikonik <blind.x6@gmail.com>
2016-03-13 17:52:49 +0100

    add update action to user api

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,6 +1,10 @@
 class Api::V1::UsersController < BaseController
   def show
-    render json: User.find(params[:id].to_i)
+    render json: User.find(params[:id].to_i), include: ['payments']
+  end
+
+  def index
+    render json: User.find_by(email: params[:filter][:email]), include: ['place']
   end
 
   def create
@@ -12,9 +16,18 @@ class Api::V1::UsersController < BaseController
     end
   end
 
+  def update
+    user = User.find(params[:id])
+    if user.update(user_params)
+      render json: user, status: :ok
+    else
+      render json: user.errors, status: :unprocessable_entity
+    end
+  end
+
   private
 
   def user_params
-    params.require(:data).require(:attributes).permit(:email, :password)
+    ActiveModelSerializers::Deserialization.jsonapi_parse(params)
   end
 end
