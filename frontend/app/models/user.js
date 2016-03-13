7168dd1191ea128868a4746015d97d8159907c7c
Pasikonik <blind.x6@gmail.com>
2016-03-13 17:57:37 +0100

    write more logic

--- frontend/app/models/user.js
+++ frontend/app/models/user.js
@@ -1,14 +1,34 @@
 import Ember from 'ember';
 import DS from 'ember-data';
+import moment from 'moment';
 
-const { Model, attr, hasMany } = DS;
+const { Model, attr, hasMany, belongsTo } = DS;
 const { computed } = Ember;
 
 export default Model.extend({
   email: attr('string'),
   password: attr('string'),
   payments: hasMany('payment'),
+  place: belongsTo('place'),
 
+  isRenter: computed('place', function() {
+    return !!this.get('place.id')
+  }),
 
+  billSet: computed.filter('payments', function(payment) {
+    return !!payment.get('bill.id');
+  }),
+  bills: computed.sort('billSet', function(a, b) {
+    if (moment(a.get('createdAt')) > moment(b.get('createdAt'))){
+      return 1;
+    } else if (moment(a.get('createdAt')) < moment(b.get('createdAt'))){
+      return -1;
+    } else {
+      return 0;
+    }
+  }),
+  rent: computed.filter('payments', function(payment) {
+    return !!!payment.get('bill.id');
+  })
 
 });
