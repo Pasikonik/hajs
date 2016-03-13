f875f57fe2783e84272992c233e5ee7827d862b7
Pasikonik <blind.x6@gmail.com>
2016-03-13 17:51:58 +0100

    add update action to payment

--- app/controllers/api/v1/payments_controller.rb
+++ app/controllers/api/v1/payments_controller.rb
@@ -2,4 +2,19 @@ class Api::V1::PaymentsController < BaseController
   def show
     render json: Payment.find(params[:id])
   end
+
+  def update
+    payment = Payment.find(params[:id])
+    if payment.update(payment_params)
+      render json: payment, status: :ok
+    else
+      render json: { errors: payment.errors }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def payment_params
+    ActiveModelSerializers::Deserialization.jsonapi_parse(params)
+  end
 end
