d9c2a79a6690509c3e9c8f9e85d78e92ea0ff78a
Pasikonik <blind.x6@gmail.com>
2016-03-13 17:58:03 +0100

    set dashboard flow

--- frontend/app/routes/dashboard.js
+++ frontend/app/routes/dashboard.js
@@ -6,6 +6,18 @@ const { service } = Ember.inject;
 export default Ember.Route.extend(AuthenticatedRouteMixin, {
   session: service('session'),
 
-  actions: {
+  model() {
+    const email = this.get('session.userEmail');
+    const userPromise = this.store.queryRecord('user', { filter: { email: email } });
+    this.set('session.user', userPromise);
+    return userPromise;
+  },
+
+  afterModel() {
+    if (this.get('session.user.isRenter')) {
+      this.transitionTo('places.place', this.get('session.user.place.id'));
+    } else {
+      this.transitionTo('places');
+    }
   }
 });
