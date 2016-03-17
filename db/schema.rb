83a52202bbac8893909fa6f9a4ff4f1d0b40d216
Pasikonik <blind.x6@gmail.com>
2016-03-17 18:53:58 +0100

    scheme update

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160309211747) do
+ActiveRecord::Schema.define(version: 20160314184020) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,11 +19,11 @@ ActiveRecord::Schema.define(version: 20160309211747) do
   create_table "bills", force: :cascade do |t|
     t.string   "concern"
     t.integer  "amount"
+    t.integer  "place_id"
     t.date     "deadline"
     t.string   "state"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer  "place_id"
     t.integer  "payment_id"
   end
 
@@ -45,9 +45,9 @@ ActiveRecord::Schema.define(version: 20160309211747) do
     t.integer  "rent"
     t.integer  "bill_id"
     t.integer  "payer_id"
+    t.integer  "user_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
-    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -66,6 +66,10 @@ ActiveRecord::Schema.define(version: 20160309211747) do
     t.string   "authentication_token"
     t.integer  "place_id"
     t.integer  "payment_id"
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "pseudonym"
+    t.string   "avatar"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
