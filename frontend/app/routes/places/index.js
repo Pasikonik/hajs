e2af0015451beb1ac242a2ed296537ca6d3ebc1f
Pasikonik <blind.x6@gmail.com>
2016-03-17 19:10:23 +0100

    extend authenticated route mixin

--- frontend/app/routes/places/index.js
+++ frontend/app/routes/places/index.js
@@ -1,6 +1,11 @@
 import Ember from 'ember';
+import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
 
-export default Ember.Route.extend({
+export default Ember.Route.extend(AuthenticatedRouteMixin, {
+  afterModel() {
+    
+  },
+
   model() {
     return this.store.findAll('place');
   }
