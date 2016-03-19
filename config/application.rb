b07894eab1067afd071b030558b9853abb126568
Pasikonik <blind.x6@gmail.com>
2016-03-19 11:13:33 +0100

    add static assets

--- config/application.rb
+++ config/application.rb
@@ -9,6 +9,7 @@ Bundler.require(*Rails.groups)
 module Hajs
   class Application < Rails::Application
     config.active_job.queue_adapter = :sidekiq
+    config.serve_static_assets = true
 
     config.middleware.insert_before 0, "Rack::Cors" do
       allow do
