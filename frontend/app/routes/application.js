e73ccf47d0dda893418cb5ac751c8bb9ee10cc36
Pasikonik <blind.x6@gmail.com>
2016-03-20 03:32:08 +0100

    cleaning

--- frontend/app/routes/application.js
+++ frontend/app/routes/application.js
@@ -1,8 +1,5 @@
 import Ember from 'ember';
 import ApplicationRouteMixin from 'ember-simple-auth/mixins/application-route-mixin';
 
-export default Ember.Route.extend(ApplicationRouteMixin,{
-  init() {
-    this.notifications.setDefaultAutoClear(true);
-  }
+export default Ember.Route.extend(ApplicationRouteMixin, {
 });
