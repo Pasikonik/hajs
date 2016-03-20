b4251eb2c1af643f99d4b9f8ada0e0be70b2a607
Pasikonik <blind.x6@gmail.com>
2016-03-20 03:31:49 +0100

    add ember-notify

--- frontend/app/controllers/settings.js
+++ frontend/app/controllers/settings.js
@@ -1,10 +1,14 @@
 import Ember from 'ember';
 
+const { inject } = Ember;
+
 export default Ember.Controller.extend({
+  notify: inject.service(),
+
   actions: {
     submit() {
       this.get('model').save().then(() => {
-        this.notifications.success('Updated successfully!');
+        this.get('notify').success('Updated successfully!');
       });
     }
   }
