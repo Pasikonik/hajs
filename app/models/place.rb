56eaf9287e3709a1a347a00ae14810bde8caf2df
Pasikonik <blind.x6@gmail.com>
2016-03-28 16:43:37 +0200

    update place model

--- app/models/place.rb
+++ app/models/place.rb
@@ -2,5 +2,5 @@ class Place < ApplicationRecord
   has_many :users, inverse_of: :place
   has_many :bills, inverse_of: :place, dependent: :destroy
   # has_many :members, class_name: 'User', foreign_key: 'id'
-  belongs_to :payer, class_name: 'User', foreign_key: 'payer_id'
+  belongs_to :payer, class_name: 'User', foreign_key: 'payer_id', required: true
 end
