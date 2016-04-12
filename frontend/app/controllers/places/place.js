ecc39dbaae0999ae3201d3b91ee24c74e433dc62
Pasikonik <blind.x6@gmail.com>
2016-04-12 19:28:00 +0200

    update place controller

--- frontend/app/controllers/places/place.js
+++ frontend/app/controllers/places/place.js
@@ -1,4 +1,5 @@
 import Ember from 'ember';
+import moment from 'moment';
 
 const { computed, inject } = Ember;
 const { alias } = computed;
@@ -12,6 +13,21 @@ export default Ember.Controller.extend({
   place: alias('model'),
   isPayer: computed('place.payer', function() {
     return this.get('session.userEmail') === this.get('place.payer.email');
+  }),
+  currentBills: computed('place.bills', 'month', function() {
+    return this.get('place.bills').filter((bill) => {
+      return moment(bill.get('createdAt')).format('MM YYYY') === this.get('month');
+    });
+  }),
+  billIds: computed('currentBills', function() {
+    return this.get('currentBills').map((bill) => bill.get('id'));
+  }),
+  redabableMonth: computed('month', function() {
+    return this.get('momentMonth').format('MMMM YYYY');
+  }),
+  momentMonth: computed('month', function() {
+    const [month, year] = this.get('month').split(' ');
+    return moment(`${year}-${month}-01`);
   }),
 
   actions: {
@@ -55,6 +71,14 @@ export default Ember.Controller.extend({
       } else {
         console.log('unknow status');
       }
+    },
+    previousMonth() {
+      const previous = this.get('momentMonth').subtract('1', 'month');
+      this.set('month', previous.format('MM YYYY'));
+    },
+    nextMonth() {
+      const previous = this.get('momentMonth').add('1', 'month');
+      this.set('month', previous.format('MM YYYY'));
     }
   }
 
