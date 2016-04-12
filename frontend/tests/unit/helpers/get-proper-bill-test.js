e8007d41d27a9ead0a3bddd47d3707e1c3f5d229
Pasikonik <blind.x6@gmail.com>
2016-04-12 19:30:46 +0200

    some test

---
+++ frontend/tests/unit/helpers/get-proper-bill-test.js
@@ -0,0 +1,10 @@
+import { getProperBill } from 'frontend/helpers/get-proper-bill';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | get proper bill');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = getProperBill([42]);
+  assert.ok(result);
+});
