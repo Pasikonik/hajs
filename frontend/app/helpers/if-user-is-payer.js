13853bcbd39a771f97f8c3ddfda58fd43d52ee8d
Pasikonik <blind.x6@gmail.com>
2016-04-14 21:47:13 +0200

    improve payer icon display

--- frontend/app/helpers/if-user-is-payer.js
+++
@@ -1,7 +0,0 @@
-import Ember from 'ember';
-
-export function ifUserIsPayer(params) {
-  return params[0].get('email') === params[1].get('email');
-}
-
-export default Ember.Helper.helper(ifUserIsPayer);
