1cd48985af62f94c3a2eed56bbcbb0b2fb697926
Pasikonik <blind.x6@gmail.com>
2016-04-23 17:14:44 +0200

    add currentUser to session

--- frontend/app/services/session.js
+++ frontend/app/services/session.js
@@ -1,8 +1,18 @@
 import Ember from 'ember';
 import SessionService from 'ember-simple-auth/services/session';
 
-const { alias } = Ember.computed;
+const { computed, inject } = Ember;
+const { alias } = computed;
 
 export default SessionService.extend({
-  userEmail: alias('data.authenticated.email')
+  store: inject.service(),
+
+  userEmail: alias('data.authenticated.email'),
+
+  currentUser: computed('data.authenticated', function() {
+    const users =  this.get('store').peekAll('user');
+    return users.find((user) => {
+      return user.get('email') === this.get('userEmail');
+    });
+  })
 });
