b25ef6b487bce7ef0bf8a628d3069b12ae790ee4
Pasikonik <blind.x6@gmail.com>
2016-05-20 19:36:11 +0200

    add module for acceptance

---
+++ frontend/tests/unit/helpers/get-this-dept-test.js
@@ -0,0 +1,10 @@
+import { getThisDept } from 'frontend/helpers/get-this-dept';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | get this dept');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = getThisDept([42]);
+  assert.ok(result);
+});
