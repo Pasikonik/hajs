4eb84a570a162e97465e95f697f0c4f44beeae3c
Pasikonik <blind.x6@gmail.com>
2016-05-24 19:49:15 +0200

    add new relation to bill

--- app/models/bill.rb
+++ app/models/bill.rb
@@ -1,6 +1,7 @@
 class Bill < ApplicationRecord
   belongs_to :place
   has_many :payments, dependent: :destroy
+  has_many :users, through: :payments
 
   def single_payment
     (amount / place.users.length).ceil
